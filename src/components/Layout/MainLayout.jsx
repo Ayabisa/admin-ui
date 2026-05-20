9a9c131e3e19f2fec87976990807e664d98ce028
mahdalena91861@gmail.com <mahdalena91861@gmail.com>
2026-05-20 14:35:27 +0700

    Responsive Layout &  fix case sensitive import

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react"; 
 import Logo from "../Elements/Logo";
-import Input from "../Elements/input";
+import Input from "../Elements/Input";
 
 function MainLayout(props) {
   const { children } = props;
